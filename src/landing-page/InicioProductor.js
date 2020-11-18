76ea99913f018dfe8faf8f7e232576427aeffba5
Juan Felipe Calao Pérez <jfcalao@uninorte.edu.co>
2020-11-18 13:33:17 -0500

    productos de usuario

--- src/landing-page/InicioProductor.js
+++ src/landing-page/InicioProductor.js
@@ -6,8 +6,9 @@ import tomato from "../images/tomato.jpg";
 import artesania from "../images/artesania.jpg";
 import Producto from "./Producto";
 import Servicio from "./Servicio";
+import { connect } from 'react-redux'
 
-function InicioProd() {
+function InicioProd(props) {
   return (
     <div className=" ">
       <div className="titulo-usuario">
@@ -20,27 +21,23 @@ function InicioProd() {
         <div className="container mt-5 mb-5 pb-5">
           <h2 className="titulo-usuario"> Productos de tu catalogo </h2>
           <div className="row mb-6">
-            <div className="col-lg-2 mt-5">
-              <Producto
-                className="pb-10"
-                nombre={"Mazorca de Maiz"}
-                valor={"400"}
-                url={fondomaiz}
-                medida={"unidad"}
-                cantidad={"30"}
-                descripcion={"Envio gratis Ponedera"}
-              ></Producto>
-            </div>
-            <div className="col-lg-2  mt-5">
-              <Producto
-                nombre={"Huevo frito"}
-                valor={"500"}
-                url={tomato}
-                medida={"libra"}
-                cantidad={"20"}
-                descripcion={"Envio gratis Ponedera"}
-              ></Producto>
-            </div>
+
+            {props.products.map((item) => {
+              return (<div className="col-lg-2 mt-5">
+                <Producto
+                  className="pb-10"
+                  nombre={item.nombre}
+                  valor={item.valor_unitario}
+                  url={item.images || fondomaiz}
+                  medida={item.unidad_medida}
+                  cantidad={item.cantidad_disponible}
+                  descripcion={item.descripcion}
+                ></Producto>
+              </div>)
+
+
+            })}
+
           </div>
         </div>
         <div className="black">
@@ -75,5 +72,10 @@ function InicioProd() {
     </div>
   );
 }
-
-export default InicioProd;
+const mapStateToProps = state => ({
+  token: state.token,
+  products: state.products,
+  productsFiltered: state.productsFiltered,
+  encontrado: state.encontrado
+})
+export default connect(mapStateToProps)(InicioProd);
