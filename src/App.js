f1c75d98bfc1e890fd4e33e3d1aa53c27418ab5e
juandavid716 <jbojato@uninorte.edu.co>
2020-11-18 00:53:04 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Registro from "./landing-page/Registro";
 import Login from "./landing-page/Login";
 import Comprador from "./landing-page/CompradorTest";
 import InicioProductor from "./landing-page/InicioProductor";
-
+import Perfil from "./landing-page/Perfil";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
         <Route exact path="/registro" component={Registro} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/comprador" component={Comprador} />
+        <Route exact path="/perfil" component={Perfil} />
         <Route exact path="/" component={Landing} />
         <Route exact path="/inicioproductor" component={InicioProductor} />
       </Switch>
